9fb3b4ae74933f02ee6365c8b3844f10e77865c2
lifeline <fmy.zikzak@yandex.ru>
2023-12-10 19:35:33 +0300

    tests finally done, temp files deleted

--- 2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-file=-f "./files1/1"
-echo $file
--- test.sh
+++ test.sh
@@ -1,122 +1,98 @@
 #!/bin/bash
 
-./1.sh ./files1/1 > /dev/null
+
+
+./comparator3.sh ./files1/1 > /dev/null
 
 var1=$?
 if [[ "$var1" == "1" ]]; then
-    printf "\033[92mTEST 1: TEST PASSED\n"
+    printf "\033[92mTEST 1.1: TEST PASSED (количество переданных аргументов (указан 1 аргумент))\n"
 else
-    printf "\033[91mTEST  1:  TEST FAILED\n"
+    printf "\033[91mTEST  1.1:  TEST FAILED (количество переданных аргументов (указан 1 аргумент))\n"
 fi
 
-./1.sh ./files1/1 ./files2/2 > /dev/null
+./comparator3.sh ./files1/1 ./files2/2 > /dev/null
 
 var1=$?
 if [[ "$var1" == "1" ]]; then
-    printf "\033[92mTEST 2: TEST PASSED\n"
+    printf "\033[92mTEST 1.2: TEST PASSED (количество переданных аргументов (указано 2 аргумента))\n"
 else
-    printf "\033[91mTEST  2:  TEST FAILED\n"
+    printf "\033[91mTEST  1.2: TEST FAILED  (количество переданных аргументов (указано 2 аргумента))\n"
 fi
 
 
 
-./1.sh ./files1/folder1 ./files2/folder2 > /dev/null
+./comparator3.sh ./files1/1 ./files2/2 -m > /dev/null
+var1=$?
+if [[ "$var1" == "1" ]]; then
+    printf "\033[92mTEST 1.3: TEST PASSED (количество переданных аргументов (указано 3 аргумента))\n"
+else
+    printf "\033[91mTEST  1.3:  TEST FAILED (количество переданных аргументов (указано 3 аргумента))\n"
+fi
+
+
+./comparator3.sh ./files1/folder1 ./files2/1 -m > /dev/null
 var1=$?
 if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 3: TEST PASSED\n"
+    printf "\033[92mTEST 2.1: TEST PASSED (тип первого аргумента: dir, второго: file)\n"
 else
-    printf "\033[91mTEST  3:  TEST FAILED\n"
+    printf "\033[91mTEST  2.1:  TEST FAILED (тип первого аргумента: dir, второго: file)\n"
 fi
 
-./1.sh ./files1/1 ./files2/1 -m > /dev/null
+
+./comparator3.sh ./files1/1 ./files2/folder1 > /dev/null
 var1=$?
 if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 4: TEST PASSED\n"
+    printf "\033[92mTEST 2.2: TEST PASSED (тип первого аргумента: file, второй: dir)\n"
 else
-    printf "\033[91mTEST  4:  TEST FAILED\n"
+    printf "\033[91mTEST  2.2:  TEST FAILED (тип первого аргумента: file, второй: dir)\n"
 fi
 
 
-./1.sh ./files1/folder1 ./files2/folder2 > /dev/null
+./comparator3.sh ./files1/1 ./files2/2 > /dev/null
 var1=$?
 if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 3: TEST PASSED\n"
+    printf "\033[92mTEST 2.3: TEST PASSED (тип первого аргумента: file, второй: file)\n"
 else
-    printf "\033[91mTEST  3:  TEST FAILED\n"
+    printf "\033[91mTEST  2.3:  TEST FAILED (тип первого аргумента: file, второй: file)\n"
 fi
 
-./1.sh ./files1/1 ./files2/1 -m > /dev/null
+
+
+
+./comparator3.sh ./files1/1 ./files2/2 -m > /dev/null
 var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 4: TEST PASSED\n"
+if [[ "$var1" == "3" ]]; then
+    printf "\033[92mTEST 3.1: TEST PASSED (проверка на правильность 3-его аргумента (указан -m(неправильно)))\n"
 else
-    printf "\033[91mTEST  4:  TEST FAILED\n"
+    printf "\033[91mTEST  3.1:  TEST FAILED (проверка на правильность 3-его аргумента (указан -m(неправильно)))\n"
 fi
 
 
-./1.sh ./files1/folder1 ./files2/folder2 -m > /dev/null
+./comparator3.sh ./files1/1 ./files2/2 -v > /dev/null
 var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 5: TEST PASSED\n"
+if [[ "$var1" == "3" ]]; then
+    printf "\033[92mTEST 3.2: TEST PASSED (проверка на правильность 3-его аргумента (указан -v(верно)))\n"
 else
-    printf "\033[91mTEST  5:  TEST FAILED\n"
-fi
-
-./1.sh ./files1/1 ./files2/1 -v > /dev/null
-var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 6: TEST PASSED\n"
-else
-    printf "\033[91mTEST  6:  TEST FAILED\n"
+    printf "\033[91mTEST  3.2:  TEST FAILED (проверка на правильность 3-его аргумента (указан -v(верно)))\n"
 fi
 
 
-./1.sh ./files1/folder1 ./files2/folder2 -m > /dev/null
-var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 5: TEST PASSED\n"
-else
-    printf "\033[91mTEST  5:  TEST FAILED\n"
-fi
-
-./1.sh ./files1/1 ./files2/1 -v > /dev/null
-var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 6: TEST PASSED\n"
-else
-    printf "\033[91mTEST  6:  TEST FAILED\n"
-fi
-
-
-./1.sh ./files1/1 ./files2/1 -v > /dev/null
+./comparator3.sh ./files1/1 ./files2/1 -v > /dev/null
 var1=$?
 if [[ "$var1" == "0" ]]; then
-    printf "\033[92mTEST 7: TEST PASSED\n"
+    printf "\033[92mTEST 4.1: TEST PASSED (сравнение числовых значений без молчаливого режима)\n"
 else
-    printf "\033[91mTEST  7:  TEST FAILED\n"
+    printf "\033[91mTEST  4.1:  TEST FAILED(сравнение числовых значений без молчаливого режима)\n"
 fi
 
-./1.sh ./files1/2 ./files2/2 -v > /dev/null
+
+./comparator3.sh ./files1/1 ./files2/2 -v > /dev/null
 var1=$?
-if [[ "$var1" == "0" ]]; then
-    printf "\033[92mTEST 8: TEST PASSED\n"
+if [[ "$var1" == "4" ]]; then
+    printf "\033[92mTEST 4.2: TEST PASSED (сравнение числовых значений без молчаливого режима)\n"
 else
-    printf "\033[91mTEST  8:  TEST FAILED\n"
+    printf "\033[91mTEST  4.2:  TEST FAILED \n"
 fi
 
 
-./1.sh ./files1/1 -v > /dev/null
-var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 9: TEST PASSED\n"
-else
-    printf "\033[91mTEST  9:  TEST FAILED\n"
-fi
-
-./1.sh ./files1/2 -v > /dev/null
-var1=$?
-if [[ "$var1" == "2" ]]; then
-    printf "\033[92mTEST 10: TEST PASSED\n"
-else
-    printf "\033[91mTEST  10:  TEST FAILED\n"
-fi
